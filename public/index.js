37aac84c0aa0d97f21cf63c1b24dd3a92015b1f4
alex <asork42@berkeley.edu>
2016-03-07 18:11:11 -0800

    removed react for now

--- public/index.js
+++ public/index.js
@@ -1,61 +1,84 @@
-// var TodoList = React.createClass({
-//   displayName: 'TodoList',
+var TodoList = React.createClass({
+  displayName: 'TodoList',
 
-//   render: function render() {
-//     var createItem = function createItem(item) {
-//       return React.createElement(
-//         'li',
-//         { key: item.id },
-//         item.text
-//       );
-//     };
+  render: function render() {
+    var createItem = function createItem(item) {
+      return React.createElement(
+        'li',
+        { key: item.id },
+        item.text
+      );
+    };
+    return React.createElement(
+      'ul',
+      null,
+      this.props.items.map(createItem)
+    );
+  }
+});
+var todoapp = React.createClass({
+  displayName: 'TodoApp',
+
+  getInitialState: function getInitialState() {
+    return { items: [], text: '' };
+  },
+  onChange: function onChange(e) {
+    this.setState({ text: e.target.value });
+  },
+  handleSubmit: function handleSubmit(e) {
+    e.preventDefault();
+    var nextItems = this.state.items.concat([{ text: this.state.text, id: Date.now() }]);
+    var nextText = '';
+    this.setState({ items: nextItems, text: nextText });
+  },
+  render: function render() {
+    return React.createElement(
+      'div',
+      null,
+      // React.createElement(
+      //   null,
+      //   'TODO'
+      // ),
+      React.createElement(TodoList, { items: this.state.items }),
+      React.createElement(
+        'form',
+        { onSubmit: this.handleSubmit },
+        React.createElement('input', { onChange: this.onChange, value: this.state.text }),
+        React.createElement(
+          'button',
+          null,
+          'Add #' + (this.state.items.length + 1)
+        )
+      )
+    );
+  }
+});
+
+// var about = React.createClass({
+//   displayName: "About",
+
+//   // getInitialState: function getInitialState() {
+//   //   return { about: };
+//   // },
+
+//   render: function render(){
 //     return React.createElement(
-//       'ul',
-//       null,
-//       this.props.items.map(createItem)
+//       "div",
+//       {className:"detail"},
+//       "My Name is Alex"
 //     );
 //   }
 // });
-// var TodoApp = React.createClass({
-//   displayName: 'TodoApp',
 
-//   getInitialState: function getInitialState() {
-//     return { items: [], text: '' };
-//   },
-//   onChange: function onChange(e) {
-//     this.setState({ text: e.target.value });
-//   },
-//   handleSubmit: function handleSubmit(e) {
-//     e.preventDefault();
-//     var nextItems = this.state.items.concat([{ text: this.state.text, id: Date.now() }]);
-//     var nextText = '';
-//     this.setState({ items: nextItems, text: nextText });
-//   },
-//   render: function render() {
-//     return React.createElement(
-//       'div',
-//       null,
-//       React.createElement(
-//         'h3',
-//         null,
-//         'TODO'
-//       ),
-//       React.createElement(TodoList, { items: this.state.items }),
-//       React.createElement(
-//         'form',
-//         { onSubmit: this.handleSubmit },
-//         React.createElement('input', { onChange: this.onChange, value: this.state.text }),
-//         React.createElement(
-//           'button',
-//           null,
-//           'Add #' + (this.state.items.length + 1)
-//         )
-//       )
-//     );
+// var aboutinfo = React.CreateClass({
+//   displayName: "Aboutinfo"
+
+//   render:function render(){
+
 //   }
-// });
+// })
 
-var Timer = React.createClass({
+var timer = React.createClass({
   displayName: "Timer",
 
   getInitialState: function getInitialState() {
@@ -80,7 +103,10 @@ var Timer = React.createClass({
   }
 });
 
-ReactDOM.render(React.createElement(Timer, null), document.getElementById('reacts'));
+
+ReactDOM.render(React.createElement(timer,null), document.getElementById('reacts0'));
+ReactDOM.render(React.createElement(todoapp,null), document.getElementById('reacts1'));
+
 
 
 
